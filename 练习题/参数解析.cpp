ab97fd7522774a35fd1d54661d92d59c8f097e56
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2019-11-20 22:30:32 +0800

    updata

--- 练习题/参数解析.cpp
+++ 练习题/参数解析.cpp
@@ -17,14 +17,34 @@
 //1.通过getline获取一行输入
 //2.创建一个vector<string>保存用空格分开的字符串
 //3.通过循环向vectotr里面插入
-#include<iostream>
-#include<string>
-#include<vector>
-using namespace std;
-int main()
-{
-	string s;
-	getline(cin, s);
-	vector<string> s;
-
-}
+//#include<iostream>
+//#include<string>
+//#include<vector>
+//using namespace std;
+//int main()
+//{
+//	string s;
+//	getline(cin, s);
+//	vector<string> v;
+//	auto start = s.begin();
+//	auto end = s.begin();
+//	while (start != s.end())
+//	{
+//		while (end != s.end() && *end != ' ')
+//		{
+//			end++;
+//		}
+//		string tmp(start, end);
+//		v.push_back(tmp);
+//		if (end != s.end())
+//		{
+//			end++;
+//		}
+//		start = end;
+//	}
+//	cout << v.size() << endl;
+//	for (int i = 0; i < v.size(); i++)
+//	{
+//		cout << v[i] << endl;
+//	}
+//}
