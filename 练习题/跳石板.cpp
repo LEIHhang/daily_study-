563cf9c2e0b9c24edfc40e9ef389c9e4e2cb55c0
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2019-11-21 19:36:36 +0800

    updata

--- 练习题/跳石板.cpp
+++ 练习题/跳石板.cpp
@@ -4,50 +4,50 @@
 //N = 4，M = 24：
 //4->6->8->12->18->24
 //于是小易最少需要跳跃5次，就可以从4号石板跳到24号石板
-#include<iostream>
-#include<vector>
-#define INT_MAX 0x7fffffff
-using namespace std;
-bool get_factor(int m,vector<int>& v)
-{
-	if (m <= 2)
-		return true;
-	for (int i = 2; i < m; i++)
-	{
-		if (m%i == 0)
-		{
-			v.push_back(i);
-		}
-	}
-	return true;
-}
-int jump(int n, int m)
-{
-	vector<int> arr(m-n+1,INT_MAX);
-
-	arr[0] = 0;//到自己需要0步
-	for (int i = 0; i < m - n + 1; i++)
-	{
-		if (arr[i] != INT_MAX)
-		{
-			//获取因数集合
-			vector<int> v;
-			get_factor(n + i,v);
-			for (int j = 0; j < v.size() ; j++)
-			{
-				if (i + v[j] < m - n + 1 && arr[i] + 1<arr[i + v[j]])
-					arr[i + v[j]] = arr[i] + 1;
-			}
-		}
-	}
-	if (arr[m - n] != INT_MAX)
-		return arr[m - n];
-	else
-		return -1;
-}
-int main()
-{
-	int n, m;
-	cin >> n >> m;
-	cout << jump(n, m);
-}
+//#include<iostream>
+//#include<vector>
+//#define INT_MAX 0x7fffffff
+//using namespace std;
+//bool get_factor(int m,vector<int>& v)
+//{
+//	if (m <= 2)
+//		return true;
+//	for (int i = 2; i < m; i++)
+//	{
+//		if (m%i == 0)
+//		{
+//			v.push_back(i);
+//		}
+//	}
+//	return true;
+//}
+//int jump(int n, int m)
+//{
+//	vector<int> arr(m-n+1,INT_MAX);
+//
+//	arr[0] = 0;//到自己需要0步
+//	for (int i = 0; i < m - n + 1; i++)
+//	{
+//		if (arr[i] != INT_MAX)
+//		{
+//			//获取因数集合
+//			vector<int> v;
+//			get_factor(n + i,v);
+//			for (int j = 0; j < v.size() ; j++)
+//			{
+//				if (i + v[j] < m - n + 1 && arr[i] + 1<arr[i + v[j]])
+//					arr[i + v[j]] = arr[i] + 1;
+//			}
+//		}
+//	}
+//	if (arr[m - n] != INT_MAX)
+//		return arr[m - n];
+//	else
+//		return -1;
+//}
+//int main()
+//{
+//	int n, m;
+//	cin >> n >> m;
+//	cout << jump(n, m);
+//}
