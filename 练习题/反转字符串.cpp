59ce973d8b0ed74d5dd03424326c77a9384249cd
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2019-11-30 14:58:30 +0800

    updata

---
+++ 练习题/反转字符串.cpp
@@ -0,0 +1,14 @@
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	string str1;
+//	cin >> str1;
+//	string str2;
+//	str1.assign(str1.rbegin(), str1.rend());
+//	for (auto e:str1)
+//	{
+//		cout << e;
+//	}
+//}
