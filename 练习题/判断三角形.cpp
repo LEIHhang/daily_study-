eb816d1cf923f6d6d325d4594b19a85558553b24
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2019-12-06 19:51:25 +0800

    updata

---
+++ 练习题/判断三角形.cpp
@@ -0,0 +1,13 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b, c;
+//	while (cin >> a >> b >> c)
+//	{
+//		if ((a+c>b) || (a+b>c)||(b+c>a))
+//			cout << "Yes" << endl;
+//		else
+//			cout << "No" << endl;
+//	}
+//}
