9d3d6233960ddbdd3761ae543fb73ee15160ab92
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2019-12-09 23:24:59 +0800

    updaya

---
+++ 练习题/正数数组的最小不可组成和.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	/**
+	*	正数数组中的最小不可组成和
+	*	输入：正数数组arr
+	*	返回：正数数组中的最小不可组成和
+	*/
+	int getFirstUnFormedNum(vector<int> arr, int len) 
+	{
+
+	}
+};
