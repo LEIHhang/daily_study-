cc6d57d9d2e1d53f678eb474ca18373d0d502de0
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2019-12-14 15:54:09 +0800

    up

--- 练习题/正数数组的最小不可组成和.cpp
+++ 练习题/正数数组的最小不可组成和.cpp
@@ -1,16 +1,27 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
-class Solution {
-public:
-	/**
-	*	正数数组中的最小不可组成和
-	*	输入：正数数组arr
-	*	返回：正数数组中的最小不可组成和
-	*/
-	int getFirstUnFormedNum(vector<int> arr, int len) 
-	{
-
-	}
-};
+//#include<iostream>
+//#include<vector>
+//using namespace std;
+//
+//class Solution {
+//public:
+//	/**
+//	*	正数数组中的最小不可组成和
+//	*	输入：正数数组arr
+//	*	返回：正数数组中的最小不可组成和
+//	*/
+//	//动态规划：
+//	//状态定义：
+//	//状态间的转移方程：
+//	//状态的初始化：
+//	//返回结果：
+//	int getFirstUnFormedNum(vector<int> arr, int len) 
+//	{
+//		int sum = 0;
+//		for (int i = 0; i < len; i++)
+//		{
+//			sum += arr[i];
+//		}
+//		bool barr = new bool[sum]();
+//		for ()
+//	}
+//};
