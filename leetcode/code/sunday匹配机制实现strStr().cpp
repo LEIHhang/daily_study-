085354616daf6f7019583e208b1a8123e15416de
LEIHhang <48359244+LEIHhang@users.noreply.github.com>
2020-05-18 22:15:56 +0800

    updata

---
+++ leetcode/code/sunday匹配机制实现strStr().cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        //sunday匹配机制
+        int L=haystack.size();
+        int N = needle.size();
+        //1.建立映射表
+        unordered_map<char,int> offset_table;
+        for(int i=0;i<N;i++)
+        {
+            offset_table[needle[i]]=N-i;
+        }
+        //2.遍历
+        int i = 0;
+        while(i<=L-N)
+        {
+            if(haystack.substr(i,N)==needle) return i;
+            else
+            {
+                //查询substr后的字符
+                if(i+N>L-1) return -1; //说明后面没有数据了
+                else
+                {
+                    if(offset_table.find(haystack[i+N])!=offset_table.end())
+                    {
+                        i+=offset_table[haystack[i+N]];
+                    }
+                    else
+                    {
+                        i+=N+1;
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
